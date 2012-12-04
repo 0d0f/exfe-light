de27b1f268e14d37957a27c3dd172454061d7d17
Leask Huang <i@leaskh.com>
2012-12-04 14:22:10 +0800

    working on mobile.js (cherry picked from commit 54b723d9d6586bc553bb6ea6f11a5c813058605c)

--- jslib/mobile/lib/mobile.js
+++ jslib/mobile/lib/mobile.js
@@ -1,5 +1,89 @@
 define(function (require, exports, module) {
 
-    alert('test');
+    require('zepto');
+
+    var config = require('config');
+
+    var home = function() {
+        // default home page
+    };
+
+    var cross = function() {
+        // please download EXFE app to view the cross
+    };
+
+    var inputPassword = function(token) {
+        $('#app-main').html(
+            '<div>'
+          +     'Password: <input type="text" id="password">'
+          +     '<button id="submit">Submit</button>'
+          + '<div>'
+        );
+        $('#submit').click(function() {
+            var password = $('#password').val();
+            if (password.length >= 4) {
+                $.ajax({
+                    type    : 'post',
+                    url     : config.api_url + '/Users/ResetPassword',
+                    data    : {token : token, password : password},
+                    success : function(data) {
+                        if (data && (data = JSON.parse(data)) && data.meta.code === 200) {
+                            if (data.response.authorization) {
+                                console.log(data.response.authorization);
+                                alert('Done');
+                            }
+                        }
+                    },
+                    error   : function() {
+                        // token 无效
+                    }
+                });
+            } else {
+                alert('Password must be longer than four!');
+            }
+        });
+    };
+
+    var resolvetoken = function(token) {
+        $.ajax({
+            type    : 'post',
+            url     : config.api_url + '/Users/ResolveToken',
+            data    : {token : token},
+            success : function(data) {
+                if (data && (data = JSON.parse(data)) && data.meta.code === 200) {
+                    switch (data.response.token_type) {
+                        case 'VERIFY':
+                            //
+                            break;
+                        case 'SET_PASSWORD':
+                            //
+                    }
+                    switch (data.response.action) {
+                        case 'VERIFIED':
+                            //
+                            break;
+                        case 'INPUT_NEW_PASSWORD':
+                            inputPassword(token);
+                    }
+                }
+            },
+            error   : function() {
+                // token 无效
+            }
+        });
+    };
+
+    var hash = document.location.hash.replace(/^#/, '');
+    if (!hash) {
+        home();
+    }
+    hash = hash.split('/');
+    for (var i = 0; i < hash.length; i++) {
+        if (hash[i].match(/token=.*/)) {
+            resolvetoken(hash[i].replace(/token=(.*)/, '$1', hash[i]));
+        } else if (hash[i].match(/!token=.*/)) {
+            cross();
+        }
+    }
 
 });
