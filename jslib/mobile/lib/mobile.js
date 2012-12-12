97feb93f49bc948a877309ca377c3cf9fe05ca67
Leask Huang <i@leaskh.com>
2012-12-12 17:36:53 +0800

    working on mobile version (cherry picked from commit bb7b2087f2f9270f6d79818ad59864c1cb941694)

--- jslib/mobile/lib/mobile.js
+++ jslib/mobile/lib/mobile.js
@@ -6,8 +6,36 @@ define(function (require, exports, module) {
 
     var Store  = require('store');
 
+    var setBtnPos = function(banner) {
+        var height = window.innerHeight;
+        if (height > 460) {
+            height = 460;
+        } else if (height < 350) {
+            height = 350;
+        }
+        $('html').css('min-height', height + 'px');
+        $('.dialog-box').css('min-height', window.innerHeight - (banner ? 50 : 0) + 'px');
+        $('.actions').css('top', (height - 86 - (banner ? 50 : 0)) + 'px');
+        $('.big-x').css('height', (height - 86 - (banner ? 50 : 0)) + 'px');
+        // $('#app-main').css('background-image', '-webkit-gradient(linear, 0 0, 0 100%, from(#88A9CB), to(#1C2732))');
+    };
+
     var home = function() {
-        // default home page
+        $('#app-main').html(
+            '<div class="dialog-box">'
+          +     '<div class="big-x">'
+          +         '<div class="img"><img width="182" height="182" src="/static/img/exfe.png"/></div>'
+          +     '</div>'
+          +     '<div class="actions">'
+          +         '<div class="get-button">'
+          +             '<button>Get <span class="exfe">EXFE</span> App <span class="free">free</span></button>'
+          +             '<div class="redirecting">Redirecting to EXFE app in <span class="sec">5</span>s.</div>'
+          +         '</div>'
+          +         '<div class="web-version"><span class="underline">Proceed</span> with desktop web version.</div>'
+          +     '</div>'
+          + '</div>'
+        );
+        setBtnPos();
     };
 
     var showAppInStore = function() {
@@ -32,11 +60,31 @@ define(function (require, exports, module) {
 
     var cross = function(token) {
         $('#app-main').html(
-            '<div>'
-          +     '<div id="cross_title"></div>'
-          +     '<div id="cross_desc"></div>'
-          + '<div>'
+            '<div class="top-banner">'
+          +     '<div class="center">'
+          +         '<div class="welcome">Welcome to <span class="exfe">EXFE</span></div>'
+          +         '<div class="exfe-logo">'
+          +             '<img src="/static/img/exfe.png" width="30" height="30" />'
+          +         '</div>'
+          +     '</div>'
+          + '</div>'
+          + '<div class="dialog-box">'
+          +     '<div class="base-info"><span class="by">dm.</span> sends you an invitation, engage in easily with <span class="exfe">EXFE</span> app.</div>'
+          +     '<div class="cross">'
+          +         '<div class="title">Team dinner with Bay Area friends in San Francisco</div>'
+          +         '<div class="time">6:30PM Fri, Apr 8</div>'
+          +         '<div class="place">Crab House @ Pier 39</div>'
+          +     '</div>'
+          +     '<div class="actions">'
+          +         '<div class="get-button">'
+          +             '<button>Get <span class="exfe">EXFE</span> App <span class="free">free</span></button>'
+          +             '<div class="redirecting">Redirecting to EXFE app in <span class="sec">5</span>s.</div>'
+          +         '</div>'
+          +         '<div class="web-version"><span class="underline">Proceed</span> with desktop web version.</div>'
+          +     '</div>'
+          + '</div>'
         );
+        setBtnPos(true);
         $.ajax({
             type    : 'post',
             url     : config.api_url + '/Crosses/GetCrossByInvitationToken',
@@ -75,11 +123,39 @@ define(function (require, exports, module) {
 
     var inputPassword = function(token) {
         $('#app-main').html(
-            '<div>'
-          +     'Password: <input type="text" id="password">'
-          +     '<button id="submit">Submit</button>'
+            '<div class="top-banner">'
+          +     '<div class="center">'
+          +         '<div class="welcome">Welcome to <span class="exfe">EXFE</span></div>'
+          +         '<div class="exfe-logo">'
+          +             '<img src="/static/img/exfe.png" width="30" height="30" />'
+          +         '</div>'
+          +     '</div>'
+          + '</div>'
+          + '<div class="dialog-box">'
+          +     '<div class="verify-actions">'
+          +         '<div class="identity">'
+          +             '<img class="avatar" width="40" height="40" src="" />'
+          +             '<span class="name">Steve Exfer</span>'
+          +         '</div>'
+          +         '<div>'
+          +             '<input type="text" id="password" placeholder="Set EXFE Password" />'
+          +         '<div>'
+          +         '<div class="error-info">Failed to set password. Please try later.</div>'
+          +         '<div class="done-info">'
+          +             '<span class="status">Password set successfully.</span>'
+          +             '<span class="redirect">Redirecting to app in <span class="sec">5</span>s.</span>'
+          +         '</div>'
+          +     '</div>'
+          +     '<div class="actions">'
+          +         '<div class="get-button">'
+          +             '<button>Get <span class="exfe">EXFE</span> App <span class="free">free</span></button>'
+          +             '<div class="redirecting">Redirecting to EXFE app in <span class="sec">5</span>s.</div>'
+          +         '</div>'
+          +         '<div class="web-version"><span class="underline">Proceed</span> with desktop web version.</div>'
+          +     '</div>'
           + '<div>'
         );
+        setBtnPos(true);
         $('#submit').click(function() {
             var password = $('#password').val();
             if (password.length >= 4) {
@@ -105,6 +181,42 @@ define(function (require, exports, module) {
                 alert('Password must be longer than four!');
             }
         });
+    };
+
+    var verification = function() {
+        $('#app-main').html(
+              '<div class="top-banner">'
+          +     '<div class="center">'
+          +         '<div class="welcome">Welcome to <span class="exfe">EXFE</span></div>'
+          +         '<div class="exfe-logo">'
+          +             '<img src="/static/img/exfe.png" width="30" height="30" />'
+          +         '</div>'
+          +     '</div>'
+          + '</div>'
+          + '<div class="dialog-box">'
+          +     '<div class="verify-actions">'
+          +         '<div class="identity">'
+          +             '<img class="avatar" width="40" height="40" src="" />'
+          +             '<img class="provider" width="18" height="18" src="" />'
+          +             '<span class="name">Steve Exfer</span>'
+          +         '</div>'
+          +         '<div class="done-info">'
+          +             '<span class="status">Verification succeeded.</span>'
+          +             '<span class="status error">Sorry, this link is expired.</span>'
+          +             '<span class="redirect">Redirecting to app in <span class="sec">5</span>s.</span>'
+          +         '</div>'
+          +     '</div>'
+          +     '<div class="space"></div>'
+          +     '<div class="actions">'
+          +         '<div class="get-button">'
+          +             '<button>Get <span class="exfe">EXFE</span> App <span class="free">free</span></button>'
+          +             '<div class="redirecting">Redirecting to EXFE app in <span class="sec">5</span>s.</div>'
+          +         '</div>'
+          +         '<div class="web-version"><span class="underline">Proceed</span> with desktop web version.</div>'
+          +     '</div>'
+          + '<div>'
+        );
+        setBtnPos(true);
     };
 
     var resolvetoken = function(token) {
@@ -137,18 +249,23 @@ define(function (require, exports, module) {
     };
 
     var hash = document.location.hash.replace(/^#/, '');
-    if (!hash) {
-        home();
-    }
-    hash = hash.split('/');
-    for (var i = 0; i < hash.length; i++) {
-        if (hash[i].match(/\/token=.*/)) {
-            resolvetoken(hash[i].replace(/\/token=(.*)/, '$1', hash[i]));
-        } else if (hash[i].match(/\!token=.*/)) {
-            cross(hash[i].replace(/\!token=(.*)/, '$1', hash[i]));
-        } else {
-            // 404
+    if (hash) {
+        hash = hash.split('/');
+        for (var i = 0; i < hash.length; i++) {
+            if (hash[i].match(/\/token=.*/)) {
+                resolvetoken(hash[i].replace(/\/token=(.*)/, '$1', hash[i]));
+            } else if (hash[i].match(/\!token=.*/)) {
+                cross(hash[i].replace(/\!token=(.*)/, '$1', hash[i]));
+            } else {
+                // 404
+            }
         }
+    } else {
+        // home();
+        cross();
+        // console.log(window.size);
+        // inputPassword();
+        // verification();
     }
 
 });
