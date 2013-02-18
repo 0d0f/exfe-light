5e1a49ade9ae39bdfaea183ea3e75c5cc48659df
Leask Huang <i@leaskh.com>
2013-02-18 17:19:03 +0800

    use humantime replaced eftime js lib (cherry picked from commit 5d7ceee2e1210c9c51998582faa0e2074d296935)

--- jslib/mobile/lib/mobile.js
+++ jslib/mobile/lib/mobile.js
@@ -190,79 +190,9 @@ define(function (require, exports, module) {
     };
 
     var renderCrossTime = function(crossTime) {
-        function trim(string) {
-            return string ? string.replace(/^\s+|\s+$/g, '') : '';
-        }
-        function escape(html, encode) {
-            return html
-                  .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
-                  .replace(/</g, '&lt;')
-                  .replace(/>/g, '&gt;')
-                  .replace(/"/g, '&quot;')
-                  .replace(/'/g, '&#39;');
-        }
-        function getTimezone() {
-            // W3C : 'Fri Jul 06 2012 12:28:23 GMT+0800 (CST)'
-            // IE  : 'Fri Jul 6 12:28:23 UTC+0800 2012'
-            var rawTimeStr  = Date().toString(),
-                numTimezone = rawTimeStr.replace(/^.+([+-]\d{2})(\d{2}).+$/i, '$1:$2'),
-                strTimezone = rawTimeStr.replace(/^.*\(([a-z]*)\).*$/i, '$1');
-                strTimezone = strTimezone === 'UTC' || strTimezone === 'GMT' ? '' : strTimezone;
-            return numTimezone + (strTimezone === rawTimeStr ? '' : (' ' + strTimezone));
-        }
-        function getTimezoneOffset(timezone) {
-            if ((timezone = trim(timezone))) {
-                var arrTimezone = timezone.split(':');
-                if (arrTimezone.length === 2) {
-                    var intHour = parseInt(arrTimezone[0], 10) * 60 * 60,
-                        intMin  = parseInt(arrTimezone[1], 10) * 60;
-                    return intHour + (intHour > 0 ? intMin : -intMin);
-                }
-            }
-            return null;
-        }
-        var efTime = require('eftime');
-        var crossOffset = getTimezoneOffset(crossTime.begin_at.timezone),
-            timeOffset  = getTimezoneOffset(getTimezone()),
-            timevalid   = crossOffset === timeOffset && require('config').timevalid,
-            strAbsTime  = '', strRelTime = '', format = 'YYYY-MM-DD',
-            placeholder = '';
-        if (crossTime.origin) {
-            var bdate = crossTime.begin_at.date
-              , btime = crossTime.begin_at.time
-              , bzone = crossTime.begin_at.timezone;
-            if (crossTime.outputformat) {
-                strAbsTime = placeholder;
-                strRelTime = escape(crossTime.origin);
-          } else if (bdate && btime) {
-            var now = new Date()
-              , matches = bdate.match(/^\d\d\d\d/m);
-            var objMon = moment((moment.utc(
-                crossTime.begin_at.date + ' '
-              + crossTime.begin_at.time, format + ' HH:mm:ss'
-            ).unix()   + (timevalid ? 0 : (crossOffset - timeOffset))) * 1000);
-            strAbsTime = objMon.format('h:mmA on ddd, MMM D' + (matches && matches[0] == now.getFullYear() ? '' : ' YYYY'))
-                        + (timevalid ? '' : (' ' + crossTime.begin_at.timezone));;
-            strRelTime = efTime.timeAgo(bdate + ' ' + btime + ' Z', undefined, 'X');
-          } else if (bdate && !btime) {
-            var now = new Date()
-              , matches = bdate.match(/^\d\d\d\d/m);
-            strAbsTime = moment(bdate).format('On ddd, MMM D' + (matches && matches[0] == now.getFullYear() ? '' : ' YYYY'))
-                        + (timevalid ? '' : (' ' + Cross.time.begin_at.timezone));;
-            now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-            strRelTime = efTime.timeAgo(bdate + ' ' + bzone[0] + bzone[1] + bzone[2]  + bzone[4] + bzone[5], +now);
-            if (strRelTime === 'Seconds ago') {
-              strRelTime = 'Today';
-            }
-          } else if (!bdate && btime) {
-            strAbsTime = '';
-            strRelTime = btime;
-          }
-        } else {
-            strAbsTime = placeholder;
-            strRelTime = 'Sometime';
-        }
-        return strAbsTime ? strAbsTime : escape(crossTime.origin);
+        var humantime = require('humantime');
+        humantime.printEFTime(crossTime, 'X');
+        return humantime.content;
     };
 
     var inputPassword = function(result, token) {
