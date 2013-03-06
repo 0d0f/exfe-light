d9e56a392962b6e6996f17f98a8156f6c3bcf492
cfddream <cfddream@gmail.com>
2013-03-07 01:56:36 +0800

    updated mnmosyne

---
+++ jslib/mnemosyne/test/demo.js
@@ -0,0 +1,51 @@
+define(function (require) {
+  //console.log('Demo ...');
+  var data = JSON.parse(photos);
+  var ViewWidget = require('mnemosyne');
+  var view = new ViewWidget($('.mnem-wrapper'));
+  var slideshow = view.slideshow;
+
+  view.render(data.response.photos);
+  view.update();
+  $(window).trigger('throttledresize');
+  view.lazyLoad();
+  view.doscroll();
+  view.startShow();
+
+  $('#retypeset').click(function () {
+    view.reTypeset();
+    view.update();
+  });
+
+  $('#addphotos').click(function () {
+    view.render(data.response.photos);
+    view.update();
+  });
+
+  $('#btn-esc').click(function () {
+    console.log('esc photo');
+    slideshow.exit();
+  });
+
+  $('#btn-prev').click(function () {
+    console.log('prev photo');
+    slideshow.stop();
+    slideshow.prev();
+  });
+
+  $('#btn-next').click(function () {
+    console.log('next photo');
+    slideshow.stop();
+    slideshow.next();
+  });
+
+  $('#btn-play').click(function () {
+    console.log('play photo');
+    slideshow.play();
+  });
+
+  $('#btn-pause').click(function () {
+    console.log('stop photo');
+    slideshow.exit();
+  });
+});
