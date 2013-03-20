b4a1cded2e52b629c8b7e184a86ebb235f2f3e0a
Leask Huang <i@leaskh.com>
2013-03-21 00:37:37 +0800

    map improve in x mobile page, 5th (cherry picked from commit 939a9296372dffd86f9f53e51e22bcc116da4f45)

--- jslib/mobile/lib/mobile.js
+++ jslib/mobile/lib/mobile.js
@@ -284,9 +284,9 @@ define(function (require, exports, module) {
                         $('.map_link').attr(
                             'href', 
                             'http://maps.google.com/maps?daddr='
-                          + encodeURIComponent(placeTitle + '@'
-                          + data.response.cross.place.lat + ','
-                          + data.response.cross.place.lng)
+                          + encodeURIComponent(placeTitle) + '@'
+                          + data.response.cross.place.lat  + ','
+                          + data.response.cross.place.lng
                         );
                     } else {
                         $('.place_area .map').hide();
