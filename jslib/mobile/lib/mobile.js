75680dd344e93b05a76d382c0d017bb629174760
Leask Huang <i@leaskh.com>
2013-04-04 17:41:19 +0800

    fixed a bug in debugging live here, 2nd

--- jslib/mobile/lib/mobile.js
+++ jslib/mobile/lib/mobile.js
@@ -831,6 +831,7 @@ define(function (require, exports, module) {
 
         // @debug only {
         var myCard = Store.get('user');
+        myCard.avarar = myCard.avatar_filename;
         if (myCard) {
             Live.init(myCard, function(data) {
                 // console.log(data);
