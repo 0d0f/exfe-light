893cc6260742f7ccab5298a9a8cfe697c090c8b0
cfddream <cfddream@gmail.com>
2013-04-15 15:49:19 +0800

    added `Animation Frame` module (cherry picked from commit 89670da1cf02a938b4e0d4763b3bb0bee92daf2a)

---
+++ production/js/af/0.0.1/af.js
@@ -0,0 +1,59 @@
+/**
+ * `Animation Frame`
+ */
+
+define('af', function () {
+  'use strict';
+
+  /** Thanks to:
+   *      http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+   *      http://creativejs.com/resources/requestanimationframe/
+   *      https://github.com/component/raf/blob/master/index.js
+   *      https://gist.github.com/paulirish/1579671
+   *      https://gist.github.com/joelambert/1002116
+   */
+
+   /**
+    * `requestAnimationFram(time)`
+    * `cancelAnimationFram`
+    */
+
+  var r = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.oRequestAnimationFrame
+    || window.msRequestAnimationFrame;
+
+   /**
+    */
+
+  var c = window.cancelAnimationFrame
+    || window.webkitCancelAnimationFrame
+    || window.mozCancelAnimationFrame
+    || window.oCancelAnimationFrame
+    || window.msCancelAnimationFrame;
+
+  if (!r) {
+    var prev = 0;
+    r = function (callback/*, element*/) {
+      var curr = new Date().getTime(),
+          ms = Math.max(0, 16 - (curr - prev)),
+          id = setTimeout(function () {
+            callback(curr + ms);
+          }, ms);
+      prev = curr + ms;
+      return id;
+    };
+  }
+
+  if (!c) {
+    c = function (id) {
+      clearTimeout(id);
+    };
+  }
+
+  return {
+    request: r,
+    cancel: c
+  };
+});
