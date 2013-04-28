e4ad15183330e3845d01ddfe7335e57e347e05d4
cfddream <cfddream@gmail.com>
2013-04-28 16:51:28 +0800

    tweaks set-password style for mobile

--- production/js/mobilecontroller/0.0.1/mobilecontroller.js
+++ production/js/mobilecontroller/0.0.1/mobilecontroller.js
@@ -220,13 +220,13 @@ define('mobilecontroller', function (require, exports, module) {
 
       this.on('reset-position', function (type, top) {
         top = App.screen.height - 96 + top;
+        this.element.removeClass('hide');
         alert(top)
         /*
         this.element.css({
           position: type ? 'absolute' : 'relative',
           top:  top + 'px'
         });
-        this.element.removeClass('hide');
         */
       });
 
