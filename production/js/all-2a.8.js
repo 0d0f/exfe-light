5f037a02258144684899f39bd4898678b6e5a543
cfddream <cfddream@gmail.com>
2013-05-29 01:35:47 +0800

    update mobile js
    (cherry picked from commit d146424614df331aee6109b52cacedffa10d7a3d)
    
    Conflicts:
    	production/js/af/0.0.1/af.min.js
    	production/js/handlebars/1.0.7/handlebars.min.js
    	production/js/humantime/0.0.8/humantime.min.js
    	production/js/lightsaber/0.0.5/lightsaber.min.js
    	production/js/mobilecontroller/0.0.4/mobilecontroller.min.js
    	production/js/store/1.3.5/store.min.js
    	production/js/tween/10.0.0/tween.min.js
    	production/js/util/0.2.6/util.min.js

--- production/js/all-2a.8.js
+++ production/js/all-2a.8.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! desktop@2a.8 2013-06-03 10:06:17 */
+/*! desktop@2a.8 2013-06-03 11:06:30 */
 (function(e) {
   "use strict";
   function t(e, t, n) {
