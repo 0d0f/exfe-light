8320a6845132772b973bc01e1c41af8e28ff90b4
cfddream <cfddream@gmail.com>
2013-06-07 14:59:48 +0800

    bug fixes: mobile redirect

--- production/js/mobile-all-2a.5.js
+++ production/js/mobile-all-2a.5.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a.5 2013-06-07 02:06:37 */
+/*! mobile@2a.5 2013-06-07 02:06:42 */
 (function(t) {
   "use strict";
   function e(t, e, i) {
@@ -3953,8 +3953,8 @@ TWEEN.Tween = function(t) {
               var t = window.search.substr(1);
               t && (t = "&" + t), window.location = "/?redirect" + t + window.location.hash;
             });
-          } else 401 === a.code ? (i.html('<span class="t">Token expired.</span> Please request to reset password again.').removeClass("hide"), 
-          n.parent().addClass("hide")) : n.removeClass("disabled").prop("disabled", !0);
+          } else 401 === a.code && i.html('<span class="t">Token expired.</span> Please request to reset password again.').removeClass("hide"), 
+          n.removeClass("disabled").prop("disabled", !0);
           n.removeClass("disabled").prop("disabled", !0);
         },
         error: function() {
