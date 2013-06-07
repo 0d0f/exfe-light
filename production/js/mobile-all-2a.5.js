221e060c2116b2952a8792fdb5822ddbcbc07750
cfddream <cfddream@gmail.com>
2013-06-07 16:29:34 +0800

    bug fixes: mobile redirect
    (cherry picked from commit 07e08635d77219dce3155c61282b1c6cfbf02be5)
    
    Conflicts:
    	production/js/mobiledirector/0.0.1/mobiledirector.min.js

--- production/js/mobile-all-2a.5.js
+++ production/js/mobile-all-2a.5.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a.5 2013-06-07 07:06:30 */
+/*! mobile@2a.5 2013-06-07 07:06:23 */
 (function(t) {
   "use strict";
   function e(t, e, i) {
