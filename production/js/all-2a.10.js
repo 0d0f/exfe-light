89d03677fa4ef81ccd354caa26d0f8abe649f8b7
cfddream <cfddream@gmail.com>
2013-06-08 19:20:10 +0800

    update 1BM60
    (cherry picked from commit a6a4717f5243d1d4c24235f8fe131325ec469935)
    
    Conflicts:
    	production/css/exfe.min.css
    	production/css/exfe_dev.min.css
    	production/js/xdialog/0.1.17/xdialog.min.js

--- production/js/all-2a.10.js
+++ production/js/all-2a.10.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! desktop@2a.10 2013-06-08 09:06:49 */
+/*! desktop@2a.10 2013-06-08 09:06:07 */
 (function(e) {
   "use strict";
   function t(e, t, n) {
