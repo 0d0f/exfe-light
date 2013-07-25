27cb92591c463222a5dc8bbddf3968dd7b185a52
cfddream <cfddream@gmail.com>
2013-07-25 13:48:30 +0800

    routex x35
    (cherry picked from commit 9ba3ab337f24d5192567f0b9dc8b88dea22cad3d)
    
    Conflicts:
    	production/js/exfemobile-2a-0e61b96.js
    	production/js/exfemobile-2a-0e61b96.min.js

--- src/routexmaps/lib/routexmaps.js
+++ src/routexmaps/lib/routexmaps.js
@@ -259,7 +259,7 @@
 
   proto.addPolyline = function (data) {
     var rgba = (data.color && data.color.split(',')) || []
-      , color = rgba.length ? '#' + (+rgba[0]).toString(16) + (+rgba[1]).toString(16) + (+rgba[2]).toString(16) : '#007BFF'
+      , color = rgba.length ? '#' + ((+rgba[0]).toString(16) + '0').substr(0, 2) + ((+rgba[1]).toString(16) + '0').substr(0, 2) + ((+rgba[2]).toString(16) + '0').substr(0, 2) : '#007BFF'
       , alpha = rgba[3] || 1
       , p = new google.maps.Polyline({
             map: this.map
