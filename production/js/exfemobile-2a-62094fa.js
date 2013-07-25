d4f97e6069d81af9513f7586949ced5db313b81c
cfddream <cfddream@gmail.com>
2013-07-25 12:43:34 +0800

    rotuex x30
    (cherry picked from commit 0b31312ad7fc14f4fc686b87f23efedb20815822)
    
    Conflicts:
    	production/js/exfemobile-2a-62094fa.js
    	production/js/exfemobile-2a-62094fa.min.js

--- production/js/exfemobile-2a-62094fa.js
+++ production/js/exfemobile-2a-62094fa.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-07-25 12:07:12 */
+/*! mobile@2a 2013-07-25 12:07:10 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
