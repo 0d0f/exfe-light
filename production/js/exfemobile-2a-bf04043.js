3981a4108c1dac62c2c8690169c07389b1e9b7fe
cfddream <cfddream@gmail.com>
2013-07-30 14:01:51 +0800

    routex x49
    (cherry picked from commit 1672e58c03db5fa5be7dc0c52cfb45131a589074)
    
    Conflicts:
    	production/js/exfemobile-2a-260171a.js
    	production/js/exfemobile-2a-260171a.min.js

--- production/js/exfemobile-2a-bf04043.js
+++ production/js/exfemobile-2a-bf04043.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-07-30 02:07:15 */
+/*! mobile@2a 2013-07-30 02:07:39 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
