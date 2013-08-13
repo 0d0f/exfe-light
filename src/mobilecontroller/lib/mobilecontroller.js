7962396bb1fa58921bcba6ff0115bd81ba0434a6
cfddream <cfddream@gmail.com>
2013-08-13 16:25:24 +0800

    routex x9

--- src/mobilecontroller/lib/mobilecontroller.js
+++ src/mobilecontroller/lib/mobilecontroller.js
@@ -1884,7 +1884,7 @@ define('mobilecontroller', function (require, exports, module) {
         });
 
         self.on('show', function () {
-          alert('Weixin', self.isWeixin);
+          alert('Weixin' + self.isWeixin);
           $('html, body').css({
               'min-height': $win.height()
             //, 'overflow': 'hidden'
