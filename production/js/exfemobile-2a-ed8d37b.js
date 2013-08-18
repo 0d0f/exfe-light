6165a66d144ef055a661e7a950b22b3fa8372ca7
cfddream <cfddream@gmail.com>
2013-08-18 16:08:53 +0800

    routex x6

--- production/js/exfemobile-2a-ed8d37b.js
+++ production/js/exfemobile-2a-ed8d37b.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-18 02:08:53 */
+/*! mobile@2a 2013-08-18 04:08:15 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
