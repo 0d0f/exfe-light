4ed10106c18cc7ece5dfc866069a8226ed6305bf
cfddream <cfddream@gmail.com>
2013-08-19 10:27:53 +0800

    routex x21
    (cherry picked from commit 92919971553e78508ad35567323a13f775138668)
    
    Conflicts:
    	package.json
    	production/css/exfemobile-09c0553.min.css
    	production/css/exfemobile-0ebfb7b.css
    	production/css/exfemobile-468c898.css
    	production/css/exfemobile-57b161b.min.css
    	production/css/exfemobile-59309d0.css
    	production/css/exfemobile-66acdc2.min.css
    	production/js/exfemobile-2a-9a2a1af.js
    	production/js/exfemobile-2a-9a2a1af.min.js
    	production/package.json

--- production/js/exfemobile-2a-9a2a1af.js
+++ production/js/exfemobile-2a-9a2a1af.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-19 10:08:57 */
+/*! mobile@2a 2013-08-19 10:08:38 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
