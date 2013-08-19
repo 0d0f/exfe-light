12fc0a208972c8a2b493219f2f0b54fff6378835
cfddream <cfddream@gmail.com>
2013-08-19 22:22:02 +0800

    routex x35

--- production/js/exfemobile-2a-d854ac2.js
+++ production/js/exfemobile-2a-d854ac2.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-19 10:08:27 */
+/*! mobile@2a 2013-08-19 10:08:28 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
@@ -5557,7 +5557,7 @@ TWEEN.Tween = function(object) {
         $.ajax({
           type: "POST",
           url: apiv3_url + "/routex/users/crosses/" + this.cross_id + "?token=" + this.token,
-          data: JSON.stringify([ data ]),
+          data: JSON.stringify(data),
           success: function(data) {
             console.log("success", data);
           },
