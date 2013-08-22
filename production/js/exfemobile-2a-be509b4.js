8fbf586786ae96661faa83a635c47f8dae702e9c
cfddream <cfddream@gmail.com>
2013-08-23 04:09:04 +0800

    update routex css
    (cherry picked from commit 923b710165d14d5c6deca4635e0ee64f4ac92928)
    
    Conflicts:
    	package.json
    	production/css/exfemobile-7a4657a.min.css
    	production/css/exfemobile-7f12236.min.css
    	production/css/exfemobile-ad21bed.css
    	production/css/exfemobile-c1bb973.css
    	production/css/exfemobile-dfc7cf3.min.css
    	production/css/exfemobile-e32a7af.css
    	production/js/exfemobile-2a-be509b4.js
    	production/js/exfemobile-2a-be509b4.min.js
    	production/package.json

--- production/js/exfemobile-2a-be509b4.js
+++ production/js/exfemobile-2a-be509b4.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-23 04:08:19 */
+/*! mobile@2a 2013-08-23 04:08:37 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
