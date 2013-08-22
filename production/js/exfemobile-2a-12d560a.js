4ebd71424bf930d5991ed1ff2822d54086e32236
cfddream <cfddream@gmail.com>
2013-08-23 03:55:56 +0800

    update routex css

--- production/js/exfemobile-2a-12d560a.js
+++ production/js/exfemobile-2a-12d560a.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-23 03:08:06 */
+/*! mobile@2a 2013-08-23 03:08:01 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
