4039beb2a36b0e737fc4577e4ef0e1a3fa3c29d8
cfddream <cfddream@gmail.com>
2013-08-23 13:02:11 +0800

    tweaks zIndex of Destination

--- production/js/exfemobile-2a-b741363.js
+++ production/js/exfemobile-2a-b741363.js
@@ -1,5 +1,5 @@
 /*! EXFE.COM QXdlc29tZSEgV2UncmUgaHVudGluZyB0YWxlbnRzIGxpa2UgeW91LiBQbGVhc2UgZHJvcCB1cyB5b3VyIENWIHRvIHdvcmtAZXhmZS5jb20uCg== */
-/*! mobile@2a 2013-08-23 12:08:11 */
+/*! mobile@2a 2013-08-23 01:08:27 */
 (function(context) {
   "use strict";
   function define(id, deps, factory) {
@@ -4147,15 +4147,16 @@ TWEEN.Tween = function(object) {
     if (places.hasOwnProperty(id) && (p = places[id]), p || (p = places[id] = this.addPoint(data)), 
     latlng = this.toLatLng(data.lat, data.lng), p.setPosition(latlng), p.data = data, 
     isDestination) {
-      var geoLocation = this.geoLocation;
+      var geoLocation = this.geoLocation, zIndex = MAX_INDEX;
       (!geoLocation || geoLocation && 0 == geoLocation._status) && this.panToDestination(latlng);
       var destinationPlace = this.destinationPlace;
       if (destinationPlace) {
         var cd = destinationPlace.data;
         data.updated_at > cd.updated_at ? (destinationPlace.setIcon(this.icons.placeMarker), 
-        this.destinationPlace = p, this.destinationPlace.isDestination = !0) : p.setIcon(this.icons.placeMarker);
+        this.destinationPlace = p, this.destinationPlace.isDestination = !0) : (p.setIcon(this.icons.placeMarker), 
+        zIndex -= 5);
       } else this.destinationPlace = p, this.destinationPlace.isDestination = !0;
-      p.setZIndex(MAX_INDEX);
+      p.setZIndex(zIndex);
     }
   }, proto.monit = function() {
     var uid, isme, d, gm, b, $e, tl, n, u = this.updated, bs = this.breadcrumbs, icons = this.icons, gms = this.geoMarkers, tiplines = this.tiplines, dp = this.destinationPlace, geo = this.geoLocation, myUserId = this.myUserId, curr_uid = this.uid, now = Math.round(Date.now() / 1e3);
