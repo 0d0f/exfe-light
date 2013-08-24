039f819103aa9e15754661a73dc143b5ae2c75c6
cfddream <cfddream@gmail.com>
2013-08-24 15:33:12 +0800

    tweaks panels

--- src/routexmaps/lib/routexmaps.js
+++ src/routexmaps/lib/routexmaps.js
@@ -1318,12 +1318,10 @@ define('routexmaps', function (require) {
          */
         , animation: 2
         , icon: this.icons.arrowGrey
-        /*
         , shape: {
               type: 'rect'
-            , rect: [0, 0, 22, 22]
+            , rect: [0, 0, 1, 1]
           }
-        */
         , optimized: false
       });
       geoLocation._status = 0;
