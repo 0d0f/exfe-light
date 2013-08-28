816fff5b63ee3b9b1fd630484991227642811f85
cfddream <cfddream@gmail.com>
2013-08-28 11:28:33 +0800

    tweaks #identities max-height

--- src/mobilecontroller/lib/mobilecontroller.js
+++ src/mobilecontroller/lib/mobilecontroller.js
@@ -1665,7 +1665,7 @@ define('mobilecontroller', function (require, exports, module) {
 
           $locate.css('-webkit-transform', 'translate3d(0, 0, 0)');
           $openExfe.css('-webkit-transform', 'translate3d(0, 0, 0)');
-          $('#identities').css('max-height', Math.round(height / 60) * 60 - 60 - 100 + 5);
+          $('#identities').css('max-height', Math.round(height / 60) * 60 - 60 - 50 + 5);
           //http://stackoverflow.com/questions/2740857/ipad-doesnt-trigger-resize-event-going-from-vertical-to-horizontal
           //https://gist.github.com/callmephilip/3626669
           //http://stackoverflow.com/questions/1207008/how-do-i-lock-the-orientation-to-portrait-mode-in-a-iphone-web-application
