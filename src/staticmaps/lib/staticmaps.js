748610e14ee18072d42d311e917d27f2ff779315
cfddream <cfddream@gmail.com>
2013-09-15 01:39:15 +0800

    improves staticmaps x14
    (cherry picked from commit dc70ffb825fa7ed392316c4ac161845fcaeceb9e)

--- src/staticmaps/lib/staticmaps.js
+++ src/staticmaps/lib/staticmaps.js
@@ -3,6 +3,7 @@
 define('staticmaps', function () {
   'use strict';
 
+  var MERCATOR_RANGE = 256;
   var proto;
 
 
@@ -38,6 +39,50 @@ define('staticmaps', function () {
   proto.getNorthWest = function () { return [this._northEast[0], this._southWest[1]]; };
 
   proto.getSouthEast = function () { return [this._southWest[0], this._northEast[1]]; };
+
+  // http://stackoverflow.com/questions/12507274/how-to-get-bounds-of-a-google-static-map
+  /*
+  function Projection() {
+    this.pixelOrigin_ = [MERCATOR_RANGE / 2, MERCATOR_RANGE / 2];
+    this.pixelsPerLonDegree_ = MERCATOR_RANGE / 360;
+    this.pixelsPerLonRadian_ = MERCATOR_RANGE / (2 * Math.PI);
+  }
+
+  proto = Projection.prototype;
+
+  proto.fromLatLngToPoint = function (latlng) {
+    var origin = this.pixelOrigin_;
+    var point = [];
+    point[0] = origin[0] + latLng[1] * this.pixelsPerLonDegree_;
+    // NOTE(appleton): Truncating to 0.9999 effectively limits latitude to
+    // 89.189.  This is about a third of a tile past the edge of the world tile.
+    var siny = this.bound(Math.sin(this.degreesToRadians(latLng[0])), -0.9999, 0.9999);
+    point[1] = origin[1] + 0.5 * Math.log((1 + siny) / (1 - siny)) * -this.pixelsPerLonRadian_;
+    return point;
+  };
+
+  proto.fromPointToLatLng = function (latlng) {
+    var origin = this.pixelOrigin_;
+    var lng = (point[0] - origin[0]) / this.pixelsPerLonDegree_;
+    var latRadians = (point[1] - origin[1]) / -this.pixelsPerLonRadian_;
+    var lat = this.radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
+    return [lat, lng];
+  }
+
+  proto.degreesToRadians = function (deg) {
+    return deg * (Math.PI / 180);
+  };
+
+  proto.radiansToDegrees = function (rad) {
+    return rad / (Math.PI / 180);
+  };
+
+  proto.bound = function (val, min, max) {
+    val = Math.max(val, min);
+    val = Math.min(val, max);
+    return value;
+  };
+  */
 
 
   function StaticMaps(map, uid) {
